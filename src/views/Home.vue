f9b96597751a672425140c610036dacbad7312f1
張硯涵 <qwee0424@gmail.com>
2022-03-09 17:06:18 +0800

    20220309

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <nav id="nav" class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-      <a class="navbar-brand" href="/">台灣旅遊資訊</a>
+      <a class="navbar-brand" href="#" @click.prevent="toHome()" >台灣旅遊資訊</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -63,8 +63,10 @@ export default {
       
     }
   },
-  computed: {
-    
+  methods: {
+    toHome(){
+      this.$router.push('/');
+    }
   }
 }
 </script>
